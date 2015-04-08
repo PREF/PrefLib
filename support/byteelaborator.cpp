e3ec217e11b761e049f834648916e5828d09b1e7
Dax <trogu.davide@gmail.com>
2015-04-08 10:10:05 +0200

    Implemented StringScanner

--- support/byteelaborator.cpp
+++ support/byteelaborator.cpp
@@ -3,6 +3,9 @@
 namespace PrefLib {
 namespace Support {
 
+const uint64_t ByteElaborator::MIN_LENGTH = 4;
+const uint64_t ByteElaborator::MAX_LENGTH = 256;
+
 ByteElaborator::ByteElaborator()
 {
 
@@ -32,6 +35,42 @@ void ByteElaborator::countBytes(CountResult &result, IO::DataBuffer *databuffer,
             result.MaxByte = b;
             result.MaxCount = c;
         }
+    }
+}
+
+void ByteElaborator::scanStrings(ByteElaborator::StringList &result, IO::DataBuffer *databuffer, uint64_t startoffset, uint64_t endoffset, volatile bool *cancontinue)
+{
+    uint64_t len = 0;
+    result.clear();
+
+    for(uint64_t i = startoffset; i < endoffset; i++)
+    {
+        if(cancontinue && !(*cancontinue))
+            break;
+
+        uint8_t ch = databuffer->at(i);
+
+        if(len == ByteElaborator::MAX_LENGTH)
+        {
+            result.push_back({i - len, i});
+            len = 0;
+            continue;
+        }
+
+        if(isprint(ch))
+        {
+            len++;
+            continue;
+        }
+
+        if(len >= ByteElaborator::MIN_LENGTH)
+        {
+            result.push_back({i - len, i});
+            len = 0;
+            continue;
+        }
+
+        len = 0;
     }
 }
 
--- support/byteelaborator.h
+++ support/byteelaborator.h
@@ -2,6 +2,7 @@
 #define PREFLIB_SUPPORT_BYTEELABORATOR_H
 
 #include <vector>
+#include <cctype>
 #include "../io/databuffer.h"
 
 namespace PrefLib {
@@ -11,15 +12,13 @@ using namespace Core;
 
 class ByteElaborator
 {
-    public:
+    public: // ByteElaborator::countBytes()
         typedef std::vector<uintmax_t> CountList;
+        struct CountResult { uint8_t MaxByte; uintmax_t MaxCount; CountList Counts; };
 
-        struct CountResult
-        {
-            uint8_t MaxByte;
-            uintmax_t MaxCount;
-            CountList Counts;
-        };
+    public: // ByteElaborator::scanStrings()
+        struct StringRange { uint64_t Start; uint64_t End; };
+        typedef std::vector<StringRange> StringList;
 
     private:
         ByteElaborator();
@@ -27,6 +26,11 @@ class ByteElaborator
 
     public:
         static void countBytes(CountResult& result, IO::DataBuffer *databuffer, uint64_t startoffset, uint64_t endoffset, volatile bool* cancontinue = nullptr);
+        static void scanStrings(StringList& result, IO::DataBuffer* databuffer, uint64_t startoffset, uint64_t endoffset, volatile bool* cancontinue = nullptr);
+
+    private:
+        static const uint64_t MIN_LENGTH;
+        static const uint64_t MAX_LENGTH;
 };
 
 } // namespace Support
