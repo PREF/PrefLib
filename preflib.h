b7435e5de8c86581c475f8d083c0d18d588eeb92
Dax89 <trogu.davide@gmail.com>
2017-03-26 16:41:29 +0200

    PREFLIB_VERSION is now a string

--- preflib.h
+++ preflib.h
@@ -1,7 +1,7 @@
 #ifndef PREFLIB_H
 #define PREFLIB_H
 
-#define PREFLIB_VERSION 3.0.0
+#define PREFLIB_VERSION         "3.0.0"
 #define PREFLIB_VESION_MAJOR    3
 #define PREFLIB_VESION_MINOR    0
 #define PREFLIB_VESION_REVISION 0
